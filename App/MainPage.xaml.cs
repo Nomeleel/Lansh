8903c388fff86eab8b9f45c391cef71e14dd73da
Choonlay <1019452620@qq.com>
2017-02-17 21:58:46 +0800

    Add project files.

---
+++ App/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        const string TASK_NAME = "MyTask";
+        const string ENTRY_POINT = "BackgroundTask.MyTask";
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void UnRegister(object sender, RoutedEventArgs e)
+        {
+            var task = BackgroundTaskRegistration.AllTasks.Values.FirstOrDefault(t => t.Name == TASK_NAME);
+            if (task != null)
+            {
+                task.Unregister(true);
+                await new Windows.UI.Popups.MessageDialog("卸载成功！").ShowAsync();
+            }
+        }
+        private async void Register(object sender, RoutedEventArgs e)
+        {
+            BackgroundAccessStatus res = await BackgroundExecutionManager.RequestAccessAsync();
+            //if (res == BackgroundAccessStatus.AllowedWithAlwaysOnRealTimeConnectivity || res == BackgroundAccessStatus.AllowedMayUseActiveRealTimeConnectivity)
+            if (res == BackgroundAccessStatus.AllowedSubjectToSystemPolicy)
+            {
+                var task = BackgroundTaskRegistration.AllTasks.Values.FirstOrDefault(t => t.Name == TASK_NAME);
+                if (task == null)
+                {
+                    BackgroundTaskBuilder btb = new BackgroundTaskBuilder();
+                    btb.Name = TASK_NAME;
+                    btb.TaskEntryPoint = ENTRY_POINT;
+                    btb.SetTrigger(new TimeTrigger(40, false));
+                    btb.AddCondition(new SystemCondition(SystemConditionType.UserPresent));
+                    btb.Register();
+                    await new Windows.UI.Popups.MessageDialog("注册成功！").ShowAsync();
+                }
+            }
+        }
+    }
+}
---
+++ BackgroundTask/MyTask.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.UI.Notifications;
+using Windows.Data.Xml.Dom;
+
+namespace BackgroundTask
+{
+    public sealed class MyTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            var deferral = taskInstance.GetDeferral();
+            XmlDocument doc = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText04);
+            //TODO
+            ToastNotification toast = new ToastNotification(doc);
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+            deferral.Complete();
+        }
+    }
+}
